bf1a00e96178bf310155656b108619bb43056731
cairofelipedev <cairofelipedev@gmail.com>
2021-08-25 23:31:15 -0300

    Create Origem

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,9 +10,27 @@ interface Task {
   isComplete: boolean;
 }
 
+interface Origem {
+  id: number;
+  razaoSocial: string;
+}
+
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+
+  const [origem, setOrigem] = useState<Origem[]>([]);
+  const [newRazaoSocial, setNewRazaoSocial] = useState('');
+
+  function handleNewOrigem() {
+
+    const newOrigem = {
+      id: Math.random(),
+      razaoSocial: newRazaoSocial
+    }
+    setOrigem(oldState => [...oldState, newOrigem]);
+    setNewRazaoSocial('');
+  }
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
@@ -47,28 +65,41 @@ export function TaskList() {
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>Minhas compras</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar compra"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
           <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
-            <FiCheckSquare size={16} color="#fff"/>
+            <FiCheckSquare size={16} color="#fff" />
           </button>
         </div>
       </header>
+      <header>
+        <h2>Origem da Compra</h2>
 
+        <div className="input-group-select">
+          <select value={newRazaoSocial}>
+            <option value="A">Shopping</option>
+            <option value="B">Online</option>
+            <option value="C">Mercado</option>
+          </select>
+          <button type="submit" data-testid="add-task-button" onClick={handleCreateNewTask}>
+            <FiCheckSquare size={16} color="#fff" />
+          </button>
+        </div>
+      </header>
       <main>
         <ul>
           {tasks.map(task => (
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -80,11 +111,11 @@ export function TaskList() {
               </div>
 
               <button type="button" data-testid="remove-task-button" onClick={() => handleRemoveTask(task.id)}>
-                <FiTrash size={16}/>
+                <FiTrash size={16} />
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
